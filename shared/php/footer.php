2518673cc54287e38ebf81126b503d2051b00bf5
6042272 <144230773+6042272@users.noreply.github.com>
2024-05-14 12:18:27 +0200

    Footer uitgewerkt, styled met css, nieuwe font (Titillium Web)

---
+++ shared/php/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <section class="footer_main">
+        <h1>Pixel Playground</h1>
+
+        <p>Gevestigd in Leiden - Nederland</p>
+        <p id="footer_contact">Heb je vragen? Neem contact met ons op hier: info@pixelplayground.com</p>
+        <p id="footer_copyright">Ⓒ 2024 Pixel Playground LLC. All Rights Reserved.</p>
+    </section>
+</footer>
