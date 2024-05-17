88575af96433edbdfbd853510ac2400d39739387
6042272 <144230773+6042272@users.noreply.github.com>
2024-05-17 10:07:34 +0200

    Nieuwe afbeelding, zet php om in html, slideshow voor games onder de begin achtergrond

--- paginas/home/index.php
+++ paginas/home/index.php
@@ -16,6 +16,19 @@
 
     <main>
 
+        <section class="home_begin">
+            <img src="img/achtergrond.jpeg" alt="Slideshow van games">
+
+            <section class="home_text_overlay">
+                <h1>Speel je <span style="color: rgb(250, 105, 40);">favoriete</span> games, nu in je web browser!</h1>
+                <p>Op onze website vind je diverse games, die hopelijk bij je passen.</p>
+
+                <p>Hier is een lijst van onze games die je nu gelijk kunt spelen.</p>
+                <a href="../login/index.php" class="button">Bekijk games</a>
+            </section>
+
+        </section>
+
         <?php
             $slideshowAfbeeldingen = [
                 "img/1.jpg",
@@ -26,16 +39,6 @@
             foreach ($slideshowAfbeeldingen as $afbeelding) {
                 echo '<section class="home_slideshow">';
                 echo '<img src="' . $afbeelding . '" alt="Slideshow van games">';
-            
-                echo '<section class="text_overlay">';
-                echo '<h1>Speel je <span style="color: rgb(250, 105, 40);">favoriete</span> games, nu in je web browser!</h1>';
-                echo '<p>Op onze website vind je diverse games, die hopelijk bij je passen.</p>';
-            
-                echo '<p>Hier is een lijst van onze games die je nu gelijk kunt spelen.</p>';
-                echo '<a href="../login/index.php" class="button">Bekijk games</a>';
-
-                echo '</section>';
-            
                 echo '</section>';
             }
           
