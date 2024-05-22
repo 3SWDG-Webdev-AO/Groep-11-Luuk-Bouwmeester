3dc947ab24eeb85d5382c0ca157ee20e35e067c9
6042272 <144230773+6042272@users.noreply.github.com>
2024-05-22 11:25:33 +0200

    Check of het gegokte letter in het woord zit, variabele voor het woord & de betekenis, woordenlijst json nederlands gemaakt

--- paginas/games/game/hangman/js/main.js
+++ paginas/games/game/hangman/js/main.js
@@ -14,8 +14,13 @@ function runHangMan(woordenLijst) {
     const randomIndex = Math.floor(Math.random() * woordenLijst.length);
     const geselecteerdeWoord = woordenLijst[randomIndex];
 
+    // Pak het actuele woord & hint op
+    const woord = geselecteerdeWoord.woord;
+    const betekenis = geselecteerdeWoord.betekenis;
+
     // Display de geselecteerde woord op de html
     const woordDisplay = document.getElementById("woord_display");
+    woordDisplay.textContent = woord;
 
     // Deze variabelen gaan we later aanpassen, eerst moeten we ze initializeren
     let lettersOver = geselecteerdeWoord.length;
@@ -30,8 +35,17 @@ function runHangMan(woordenLijst) {
     const raadInput = document.getElementById("raad_input");
     const raadButton = document.getElementById("raad_button");
 
+    // Event listener die wordt geroepen als je op de gok knop drukt.
     gokButton.addEventListener("click", () => {
+        // Vang de input op (en maak dit lowercase om problemen te vermijden)
         const gok = gokInput.value.toLowerCase();
+
+        // Maak de input weer leeg, voor de volgende gok
+        gokInput.value = "";
+
+        if (woord.includes(gok)) {
+            console.log("Woord zit er in");
+        }
         console.log(gok);
     });
 
