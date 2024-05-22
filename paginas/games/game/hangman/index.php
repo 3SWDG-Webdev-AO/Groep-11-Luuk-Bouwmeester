44e7c40c2b8f4b222261a9aca73928ddc9498825
6042272 <144230773+6042272@users.noreply.github.com>
2024-05-22 10:33:53 +0200

    Bestand aangemaakt voor hangman game

---
+++ paginas/games/game/hangman/index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="utf8">
+    <meta name="viewport" content="width-device-width, initial-scale=1">
+    <meta name="description" content="Hangman pagina van Pixel Playground">
+    <meta name="author" content="L. Bouwmeester">
+    <meta name="keywords" content="pixel, playground, hangman">
+    <title>Pixel Playground - Hangman</title>
+    <link rel="stylesheet" type="text/css" href="../../../../shared/css/style.css">
+    <script src="js/main.js" defer></script>
+</head>
+
+<body>
+    <?php include "../../../../shared/php/header.php"; ?>
+
+    <main>
+        
+    </main>
+
+    <?php include "../../../../shared/php/footer.php"; ?>
+</body>
+</html>
