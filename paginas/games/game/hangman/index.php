5ca035cf6b9c3346946c18edd5f1af719fb3e5f6
6042272 <144230773+6042272@users.noreply.github.com>
2024-05-24 09:34:36 +0200

    Verander html, start stylen met css

--- paginas/games/game/hangman/index.php
+++ paginas/games/game/hangman/index.php
@@ -15,23 +15,34 @@
     <?php include "../../../../shared/php/header.php"; ?>
 
     <main>
-        <p id="woord_display"></p>
-        <p id="game_over_display"></p>
+        <section class="game_container">
+            <p id="woord_display"></p>
+            <p id="game_over_display"></p>
 
-        <label for="gok_input">Gok een letter:</label>
-        <input type="text" id="gok_input" maxlength="1">
-        <button id="gok_button">Gok!</button>
+            <section class="gok_letter">
 
-        <br>
+                <label for="gok_input">Gok een letter:</label>
+                <input type="text" id="gok_input" maxlength="1">
+                <button id="gok_button">Gok!</button>
 
-        <label for="raad_input">Raad het woordt:</label>
-        <input type="text" id="raad_input">
-        <button id="raad_button">Raad!</button>
+            </section>
 
-        <br>
+            <section class="raad_woord">
 
-        <button id="hint_button">Krijg hint</button>
-        <span id="hint_display"></span>
+                <label for="raad_input">Raad het woordt:</label>
+                <input type="text" id="raad_input">
+                <button id="raad_button">Raad!</button>
+
+            </section>
+            
+            <section class="hint_woord">
+
+                <button id="hint_button">Krijg hint</button>
+                <span id="hint_display"></span>
+
+            </section>
+
+        </section>
     </main>
 
     <?php include "../../../../shared/php/footer.php"; ?>
