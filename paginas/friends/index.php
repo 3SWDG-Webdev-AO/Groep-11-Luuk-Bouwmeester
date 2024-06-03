8da4f1445201f723798d02e0e2fcc19f86b62eb6
6042272 <144230773+6042272@users.noreply.github.com>
2024-06-03 10:31:16 +0200

    Echo alle gebruikers

--- paginas/friends/index.php
+++ paginas/friends/index.php
@@ -15,6 +15,51 @@
 
     <main>
 
+        <?php
+            // Gegevens van de database
+            $host = "localhost";
+            $dbname = "pixelplayground";
+            $username = "root";
+            $password = "";
+
+            try {
+                // Connect d.m.v. pdo
+                $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+                // Set pdo error mode
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                // Bereid de query voor
+                $query = "SELECT * FROM gebruikers ORDER BY id DESC";
+
+                // Voer de query uit
+                $result = $pdo->query($query);
+
+                // Fetch de gebruikers
+                $gebruikers = $result->fetchAll(PDO::FETCH_BOTH);
+
+                // Kijk of we wel gebruikers zijn
+                if(count($gebruikers) > 0) {
+                    // Loop door alle gebruikers
+                    foreach($gebruikers as $gebruiker) {
+                        echo $gebruiker["gebruikersnaam"];
+                    }
+                } else {
+                    echo "Geen gebruikers gevonden!";
+                }
+            } catch (PDOException $e) {
+                // Catch error
+                echo "Foutmelding: " . $e->getMessage();
+            }
+
+            // Sluit connectie
+            $pdo = null;
+        ?>
+
+
+
+
+
     </main>
 
     <?php include "../../shared/php/footer.php"; ?>
