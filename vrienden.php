7e70324ab5891a0b7686e3dd57d7e8dd696b2bc9
6042272 <144230773+6042272@users.noreply.github.com>
2024-06-04 11:18:54 +0200

    Move alles naar root

--- vrienden.php
+++ vrienden.php
@@ -7,12 +7,12 @@
     <meta name="author" content="L. Bouwmeester">
     <meta name="keywords" content="pixel, playground, games">
     <title>Pixel Playground - Vrienden</title>
-    <link rel="stylesheet" type="text/css" href="../shared/css/style.css">
-    <script src="../shared/js/vrienden.js" defer></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="js/vrienden.js" defer></script>
 </head>
 
 <body>
-    <?php include "../shared/php/header.php"; ?>
+    <?php include "php/header.php"; ?>
 
     <main>
         <section class="vrienden_header">
@@ -84,6 +84,6 @@
 
     </main>
 
-    <?php include "../shared/php/footer.php"; ?>
+    <?php include "footer.php"; ?>
 </body>
 </html>
